36d7106ec66b86fa6d319fe31865e80b3a8a468b
Welllucky <maxwellucas.wl@gmail.com>
2023-03-13 00:18:38 -0300

    Aplicação do PWA

---
+++ pwabuilder-adv-sw.js
@@ -0,0 +1,3 @@
+
+  import { precacheAndRoute } from 'workbox-precaching/precacheAndRoute';
+  precacheAndRoute([{"revision":"c739e73a3cd0425539eb574aa427332c","url":"css/styles.css"},{"revision":"9cbb22c5fa12c00e486a2a20a4a6011d","url":"index.html"},{"revision":"9981c88aa014dbe667b2cbf6e3d1efae","url":"js/scripts.js"},{"revision":"62fbb80f6fd2e6c7c30310e791483b5b","url":"offline.html"},{"revision":"118a7f620074ba2b0be0298658a7f5d4","url":"service-worker.js"}]);
